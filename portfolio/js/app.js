0ac5940785c47b70310f288a014f8531dc3d50cd
seanvsville <seanvanho@gmail.com>
2016-06-10 01:31:01 -0700

    linked up

--- portfolio/js/app.js
+++ portfolio/js/app.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+  $('#side_links').on('click', function() {
+    $('.sidebar').css({
+      'left': '-190px'
+    });
+  });
+
+  $('#side_links').on('click', function() {
+    $('.hamburger').css({
+      'left': '20px'
+    });
+  });
+
+  $('.hamburger').on('click', function() {
+    console.log('hamburger clicked');
+  });
 
 
 });
